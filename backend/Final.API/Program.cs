72bd9e619b20d44662283103e34e2deb39748c50
tracewilson <wiltrac3@byu.edu>
2025-04-16 22:59:51 -0600

    cors fix

--- backend/Final.API/Program.cs
+++ backend/Final.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "http://brave-sky-05b62981e.6.azurestaticapps.net/")
             .AllowCredentials()
             .AllowAnyHeader()
             .AllowAnyMethod();
